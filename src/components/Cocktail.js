0356c1dfe702e5fad50cd40b8c9cd43cbb3a517c
thangricktran <thangricktran2@gmail.com>
2020-12-12 00:34:32 -0800

    initial commit

---
+++ src/components/Cocktail.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Cocktail = ({ id, name, image, info, glass }) => {
+  // console.log('Cocktail js all props:', id, name, image, info, glass);
+  return (
+    <article className="cocktail">
+      <div className="img-container">
+        <img src={image} alt={name} title={name} />
+      </div>
+      <div className="cocktail-footer">
+        <h3>{name}</h3>
+        <h4>{glass}</h4>
+        <p>{info}</p>
+        <Link to={`/cocktail/${id}`} className="btn btn-primary btn-details">
+          detail
+        </Link>
+      </div>
+    </article>
+  )
+}
+
+export default Cocktail
---
+++ src/pages/About.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+        Alias unde quae provident mollitia ipsum et consequatur 
+        exercitationem, molestiae eos! Cumque sint eaque perferendis 
+        iure vero minima quia nisi vel saepe. Lorem ipsum dolor sit 
+        amet consectetur, adipisicing elit. Repellendus adipisci 
+        culpa quibusdam! Assumenda voluptates blanditiis ipsa a iste 
+        eius eos omnis sapiente repudiandae reprehenderit, ullam, 
+        possimus cum temporibus quam non.
+      </p>
+    </section>
+  );
+};
+
+export default About;
